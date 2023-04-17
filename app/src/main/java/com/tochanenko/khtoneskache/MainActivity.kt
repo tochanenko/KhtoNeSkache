547c96f20cc56bcfa8cb2d7b6a8c7d7a8075dcec
Vladyslav Tochanenko <itochanenko@gmail.com>
2023-04-17 22:48:24 +0300

    Update libraries

--- app/src/main/java/com/tochanenko/khtoneskache/MainActivity.kt
+++ app/src/main/java/com/tochanenko/khtoneskache/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tochanenko.khtoneskache
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tochanenko.khtoneskache.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
